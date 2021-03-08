f3124a8ef8c3a3e8e9f142570afcd058ab1c7187
Ale Gómez <60187741+aleg001@users.noreply.github.com>
2021-03-08 00:18:50 -0600

    Se crea el main para el programa

---
+++ Main.py
@@ -0,0 +1,46 @@
+#Main.py
+#Universidad del Valle de Guatemala
+#Alejandro Gómez 20347
+#Gabriela Contreras 20213
+#Creacion: 7-03-21
+#Ultima Edicion: 7-03-21
+
+
+#import simpy
+from time import sleep
+import sys
+
+def Bienvenida():
+    Oraciones = "Bienvenido al simulador de programa de uso de colas"
+    for char in Oraciones:
+        sleep(0.005)
+        sys.stdout.write(char)
+
+def menu():
+    print("\n","\033[4m","SIMULADOR",'\033[m',"\n")
+    print (" 1. Iniciar simulación", "\n","2. Creditos", "\n", "3. Salir")
+
+def Creditos():
+    print("Creado por:")
+    print("Alejandro Gomez")
+    print("Paola Contreras")
+
+
+Bienvenida()
+print("\n")
+
+#Menu
+
+var = False
+while var == False:
+    menu()
+    opcion = int(input("\n""\033[4m""Elija la opción que quiera:"'\033[m'""))
+    if opcion == 1:
+        print("Iniciando simulacion...")
+    if opcion == 2:
+        print("\n")
+        Creditos()
+    if opcion == 3:
+        print("Saliendo el programa...")
+        print("Gracias por utilizar nuestro programa jeje")
+        var = True
